a879058bda2919868fb1995b1050b8a40bdab44c
Matthew Murphy <MATMU@MATMU-MacBook-Pro.local>
2022-05-20 16:39:54 +0100

    First push of iteration 1

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,12 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.get(/copy2021-handler/, function (req, res) {
+    if (req.query.contact === "email")  {
+     res.redirect('email');
+   } else {
+     res.redirect('check-your-answers');
+   }
+  });
+
 module.exports = router;
