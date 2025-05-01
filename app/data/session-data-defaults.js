e84e9fa716d56b252362a06b9f87155990f81707
Chukwudi Chima <chchi@nhsbsa.nhs.uk>
2025-05-01 10:41:18 +0100

    reverted 'Annual price uplift 2025' changes to previous pricing

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -64,8 +64,8 @@ function _getMedications(){
 //
 module.exports = {
 
-    NHSPrescriptionCost: '£10.15',
-    HRTPPCCost: '£20.30',
+    NHSPrescriptionCost: '£9.90',
+    HRTPPCCost: '£19.80',
 
     phaseBannerTagText: 'BETA',
     phaseBannerFeedbackLink: 'https://online1.snapsurveys.com/Interview/77e67933-70d0-410f-b972-c748a95fa0cc',
