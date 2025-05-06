9ee8778839bd4a840df6a84334a437979e158ec7
James Lattimore <james.lattimore@nhsbsa.nhs.uk>
2025-05-06 11:02:24 +0100

    Fixing the double start page issue.

--- app/views/v12-nunjucks/_routes.js
+++ app/views/v12-nunjucks/_routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.post(/start\b$/, function (req, res) {
+router.post(/start-redirect/, function (req, res) {
   req.session.data = {};
   res.redirect('is-your-medicine-covered');
 });
