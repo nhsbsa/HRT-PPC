50b2b585aa17317653998e6d63797520e93c4b8a
Chukwudi Chima <chchi@nhsbsa.nhs.uk>
2026-04-17 14:04:32 +0100

    updated service name in config file

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Service name goes here',
+  serviceName: 'Buy an NHS HRT Prescription Prepayment Certificate (HRT PPC)',
 
   // Port to run nodemon on locally
   port: 2000,
